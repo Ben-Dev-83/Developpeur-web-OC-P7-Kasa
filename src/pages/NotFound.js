d7345bad09d22759609b2a4e424397f82db9f070
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-02-17 14:48:15 +0100

    Ajout lien vers home et scss

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,12 @@
 import NotFoundStyle from '../styles/NotFound.module.scss'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
     return (
-        <div>
+        <div className={NotFoundStyle.container}>
             <h1 className={NotFoundStyle.title}>404</h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <p className={NotFoundStyle.paragraphe}>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className={NotFoundStyle.link}>Retourner sur la page d’accueil</Link>
         </div>
     )
 }
