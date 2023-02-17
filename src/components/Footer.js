bd0e97727f838fda5e99a5565eb01fd18c3331ba
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-02-17 11:56:01 +0100

    Création du footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import footerStyle from '../styles/Footer.module.scss'
+import logo from '../asset/logo-footer.png'
+
+
+function Footer() {
+    return (
+        <footer className={footerStyle.footer}>
+            <div className={footerStyle.description}>
+                <img src={logo} alt="logo footer"/> 
+                <p className={footerStyle.copyright}>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
