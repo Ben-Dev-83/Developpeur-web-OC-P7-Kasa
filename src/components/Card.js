f8ebb7abed789ce0565f264af0e56d355ef9ba70
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-02-22 08:01:38 +0100

    composant renommé

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,18 @@
 import cardStyle from'../styles/Card.module.scss'
-import announcementList from '../kasa.json'
+import {Link} from 'react-router-dom'
+import accommodationList from '../kasa.json'
+import { useNavigate } from "react-router-dom";
 
 function Card() {
+    const navigate = useNavigate();
+
     return (
         <div className={cardStyle.container}>
-            {announcementList.map((announcement, index) => (
-                <a href='' key={index} className={cardStyle.list}>
+            {accommodationList.map((announcement, index) => (
+                <Link to={`/logement/${announcement.id}`} key={index} className={cardStyle.list}>
                     <h2 className={cardStyle.title}>{announcement.title}</h2>
                     <img src={announcement.cover} alt={announcement.title} className={cardStyle.img}/>
-                </a>
+                </Link>
             ))}        
         </div>
     )
