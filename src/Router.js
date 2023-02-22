866bf29e09ffd3f7b72c26a6e424e8942bbad27f
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-02-22 07:53:49 +0100

    Router

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,20 @@
-import {Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About'
 import NotFound from './pages/NotFound'
+import Accommodation from './pages/Accommodation'
 
 function Router() {
     return (
-        <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/a-propos' element={<About/>}></Route>
-        </Routes>    
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<NotFound />} />
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/a-propos' element={<About/>}></Route>
+                <Route path='/logement/:id' element={<Accommodation/>}></Route>
+            </Routes>   
+        </BrowserRouter>
+
     )
 }
 
