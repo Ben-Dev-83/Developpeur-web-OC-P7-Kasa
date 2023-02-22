2f1ae9a7e0a856cb64148dadd435154cd4b29085
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-02-22 23:03:58 +0100

    condition collapse si dans logement ou à propos

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -8,14 +8,19 @@ import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
 function Collapse(props) {
     const [collapse, collapseOpen] = useState(false)
+
+    const height= props.styled ? CollapseStyle['height'] : ''
     return (
-        <div className={ `${CollapseStyle.container} ${collapse ? CollapseStyle.collapseOp : CollapseStyle.collapseClose}`}>
+        <div className={ ` ${height} ${CollapseStyle.container} ${collapse ? CollapseStyle.collapseOp : CollapseStyle.collapseClose}`}>
             <div className={CollapseStyle.titleContainer} onClick={() => collapseOpen(!collapse)} >
                 <h2 className={CollapseStyle.title}>{props.title}</h2>
                 <FontAwesomeIcon icon={collapse ? faChevronUp : faChevronDown} className={CollapseStyle.color} />
             </div>
-            <div className={CollapseStyle.containerDesc}>
-                <p className={CollapseStyle.containerDesc__description}>{props.text}</p>
+            <div className={Array.isArray(props.text) ? `${CollapseStyle.containerDesc} ${CollapseStyle.containerDesc__description}` : `${CollapseStyle.containerDesc__aboutDesc} ${CollapseStyle.containerDesc}`}>
+                {Array.isArray(props.text) ? props.text.map((desc, index) => (
+                    <p key={index}>{desc}</p>
+                    )) : <p>{props.text}</p>
+                }
             </div>
         </div>
     )
