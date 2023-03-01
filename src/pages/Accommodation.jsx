3bbc70b6766de1c79118cde61a8f3c6d62128853
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-03-01 08:00:54 +0100

    navigate si mauvais id

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -5,37 +5,35 @@ import Host from "../components/Host";
 import accommodationList from '../kasa.json'
 import style from '../styles/Accomodation.module.scss'
 import LayoutStyle from '../styles/AppLayout.module.scss'
-import Banner from '../components/Banner'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Tags from '../components/Tags'
 import Rate from '../components/Rate'
-
-
+import { Navigate } from "react-router-dom";
 
 function Accommodation() {
         let { id } = useParams();
         const accommodationFind = accommodationList.find((accommodation) => {
             return accommodation.id === id
         })
-
         return (
+          accommodationFind ? 
           <div className='container'>
-            <Banner/>
-            <div className={LayoutStyle.layout}>
-
+          <Header/>
+          <main className={LayoutStyle.layout}>
             <Carrousel
-                title= {accommodationFind.title}
-                pictures= {accommodationFind.pictures}
+              title= {accommodationFind.title}
+              pictures= {accommodationFind.pictures}  
             />
             <div className={style.container}>
-              <div>
+              <section>
                 <h1 className={style.title}>{accommodationFind.title}</h1>
                 <p className={style.location}>{accommodationFind.location}</p>
                 <Tags
                   tags= {accommodationFind.tags}
                   />
-              </div>
-              <div className={style.host}>
+              </section>
+              <aside className={style.container__host}>
                 <Host
                   name= {accommodationFind.host.name}
                   picture= {accommodationFind.host.picture}
@@ -43,23 +41,24 @@ function Accommodation() {
                   <Rate
                     rating= {accommodationFind.rating}
                   />
-              </div>
+              </aside>
             </div>
             <div className={style.collapse}>
               <Collapse
                 title= "Description"
                 text= {accommodationFind.description}
                 styled= {true}
-                />
+              />
               <Collapse
                 title= "Équipements"
                 text= {accommodationFind.equipments}
                 styled= {true}
-                />
+              />
             </div>
-            </div>
-            <Footer/>
-            </div>
+          </main>
+          <Footer/>
+          </div>:
+          <Navigate to="*" replace={true}/>
         );
 
 }
