0ed9cc2562e138548bc7d5e760747a768de7caa5
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-03-01 07:58:18 +0100

    effet carrousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,11 +1,12 @@
 import style from '../styles/Carrousel.module.scss'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 function Carrousel(props) {
     let  pictures = props.pictures
     let [index, setIndex] = useState(0);
+    let [loaded, setLoaded] = useState(false);
     let length = pictures.length
     const display = pictures.length === 1 ? style.hide : style.container__display
 
@@ -17,10 +18,19 @@ function Carrousel(props) {
         const newIndex = index + 1;
         setIndex(newIndex >= length ? 0 : newIndex);
     };
+    useEffect(() => {
+        if (loaded) {
+          const timeoutId = setTimeout(() => {
+            setLoaded(false);
+          }, 500);
+          return () => clearTimeout(timeoutId);
+        }
+      }, [loaded]);
+    
     return (
         <div>
             <div className={style.container}>
-                <img src={pictures[index]} alt='test' className={style.img}/>
+                <img src={pictures[index]} alt={index} className={`${style.img} ${loaded ? style.flash :"" }`} onLoad={() => setLoaded(true)}/>
                 <div className={display}>
                     <div className={style.container__icons}>
                     <FontAwesomeIcon icon={faChevronLeft} className={style.icon} onClick={handlePrevious}/>
