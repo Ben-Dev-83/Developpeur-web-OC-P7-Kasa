d77a51f0cb55e03bb8bb754a6353c7e08ce05048
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-03-03 11:56:53 +0100

    key déplacé dans article

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ function Card() {
   return (
     <section className={style.container}>
       {accommodationList.map((announcement, index) => (
-        <article key={index}>
+        <article className={style.article} key={index}>
           <Link to={`/logement/${announcement.id}`} className={style.list}>
             <h2 className={style.list__title}>{announcement.title}</h2>
             <img src={announcement.cover} alt={announcement.title} className={style.list__img}/>
