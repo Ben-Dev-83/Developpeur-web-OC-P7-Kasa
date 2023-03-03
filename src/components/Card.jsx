30f7d3c8181c4249ef1c8ef2142e810834433ff3
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-03-03 10:29:02 +0100

    indentation, key dans article

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,18 @@ import {Link} from 'react-router-dom';
 import accommodationList from '../kasa.json';
 
 function Card() {
-    return (
-        <section className={style.container}>
-            {accommodationList.map((announcement, index) => (
-                <article>
-                    <Link to={`/logement/${announcement.id}`} key={index} className={style.list}>
-                        <h2 className={style.list__title}>{announcement.title}</h2>
-                        <img src={announcement.cover} alt={announcement.title} className={style.list__img}/>
-                    </Link>
-                </article>
-            ))}        
-        </section>
-    )
+  return (
+    <section className={style.container}>
+      {accommodationList.map((announcement, index) => (
+        <article key={index}>
+          <Link to={`/logement/${announcement.id}`} className={style.list}>
+            <h2 className={style.list__title}>{announcement.title}</h2>
+            <img src={announcement.cover} alt={announcement.title} className={style.list__img}/>
+          </Link>
+        </article>
+      ))}        
+    </section>
+  )
 }
 
 export default Card;
